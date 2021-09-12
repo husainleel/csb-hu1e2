fde508f5097009f1c983e3cd4c772c5c7688c996
Code Sandbox <hello@codesandbox.io>
2021-09-12 09:57:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.7f607e85331877f28e6ff4007a6a2e73.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d18589b7e06de704a9247176f5fa255",
+    "url": "https://husainleel.github.io/csb-hu1e2/index.html"
+  },
+  {
+    "revision": "9876e6fd3cde9796e42d",
+    "url": "https://husainleel.github.io/csb-hu1e2/static/js/2.a63bfbb6.chunk.js"
+  },
+  {
+    "revision": "393fdc834b6a1666170b",
+    "url": "https://husainleel.github.io/csb-hu1e2/static/js/main.17170c20.chunk.js"
+  },
+  {
+    "revision": "6a6f334dc65cce6293ac",
+    "url": "https://husainleel.github.io/csb-hu1e2/static/js/runtime-main.68a12507.js"
+  }
+]);
